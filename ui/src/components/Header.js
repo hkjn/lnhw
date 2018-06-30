ef0940e5e1937277efdffcead97e14dca5fc7aea
GustavoRSSilva <gustavorssilva09@gmail.com>
2018-06-30 14:35:19 +0100

    [new] header

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import HomeIcon from '../assets/img/home-icon.svg';
+import DeviceIcon from '../assets/img/chip.svg';
+
+import '../assets/css/header.css';
+
 
 class Header extends Component {
     render() {
+        //  TODO add balance
+        const balance = "-";
         const connected = this.props.connected;
 
         return (
             <header>
                 <div className="header-logo" />
-                <div className="header-main" />
-                <div className="header-balance" />
+                <div className="header-main" >
+                    <div className="home-icon-container">
+                        <img src={HomeIcon} />
+                        <span>Home</span>
+                    </div>
+                    <div className="device-container">
+                        <div className="device-icon-container">
+                            <img src={DeviceIcon} />
+                            <span>Device</span>
+                        </div>
+                        <div className={`check-connected ${!connected  ? 'not-connected' : ''}`}>
+                            {connected  ? 'Connected' : 'Not connected'}
+                        </div>
+                    </div>
+                </div>
+                <div className="header-balance" >
+                    <div className="balance-container">
+                        <span>Balance</span>
+                        <label>Ƀ {balance}</label>
+                    </div>
+                </div>
             </header>
         );
     }
