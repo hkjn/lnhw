e2e7c76cd179503b682ef97379e598dd5939fd58
Justin Moen <jamoen7@gmail.com>
2018-06-30 13:11:29 -0500

    Simplify api.js and hydrate state on load.

--- ui/src/services/api.js
+++ ui/src/services/api.js
@@ -1,38 +1,36 @@
 import axios from 'axios';
 
+function rpc(method, args) {
+    return axios.post('/rpc', { method, args })
+}
+
 export function getInfo () {
-    return axios.post('/rpc', {
-        method: 'getinfo'
-    })
+    return rpc('getinfo')
 }
 
 export function pay (bolt11) {
-    return axios.post('/rpc', {
-        method: 'pay',
-        args: {
-            bolt11
-        },
-    })
+    return rpc('pay', { bolt11 })
 }
 
 export function listPeers () {
-    return axios.post('/rpc', {
-        method: 'listpeers',
-    })
+    return rpc('listpeers')
 }
 
 export function listFunds() {
-    return axios.post('/rpc', {
-        method: 'listfunds',
-    })
+    return rpc('listfunds')
 }
 
-export function fundchannel(channelId, satoshi) {
-    return axios.post('/rpc', {
-        method: 'fundchannel',
-        args: {
-            channel_id: channelId,
-            satoshi
-        }
+export function listPayments() {
+    return rpc('listpayments')
+}
+
+export function listInvoices() {
+    return rpc('listinvoices')
+}
+
+export function fundChannel(channelId, satoshi) {
+    return rpc('fundchannel', {
+        channel_id: channelId,
+        satoshi
     })
 }
