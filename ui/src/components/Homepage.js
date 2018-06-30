160cf3575605d0f23c6417fba2fd142825bfe3b1
GustavoRSSilva <gustavorssilva09@gmail.com>
2018-06-30 18:37:40 +0100

    [new] added the channel fragment

--- ui/src/components/Homepage.js
+++ ui/src/components/Homepage.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import Channel from './Channel';
+
+import '../assets/css/homepage.css';
 
 class Homepage extends Component {
     render() {
+
         return (
-            <div>
+            <div id="homepage-wrapper">
+                <div className="title-container">
+                    <div className="title-tab selected-title-tab">
+                        My channels
+                    </div>
+                    <div className="title-tab">
+                        Transaction history
+                    </div>
+                    <div className="title-tab">
+                        My bitcoin address
+                    </div>
+                </div>
+                <Channel />
                 <p>Connected to Network: {this.props.network}</p>
             </div>
         );
